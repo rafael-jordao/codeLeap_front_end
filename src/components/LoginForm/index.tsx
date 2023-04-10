90d66176291cc319bd912179b21c7d4aaa0cd9e5
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-04-10 20:31:17 -0300

    Fixing Bugs

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+
+import { FormEvent, ChangeEvent } from 'react';
+
+import Input from '../Input';
+import { Button } from '../Button';
+
+import { Wrapper, Container, Form } from './styles';
+
+import { useNavigate } from 'react-router-dom';
+
+export default function LoginForm() {
+  const [name, setName] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      navigate('/feed');
+    }
+  }, [navigate]);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const input = event.target.value;
+    setName(input);
+
+    if (input.length > 0) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }
+
+  function signUp(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    localStorage.setItem('user', name);
+    navigate('/feed');
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <h1>Welcome to CodeLeap network!</h1>
+        <Form onSubmit={signUp}>
+          <Input
+            label="Please enter your username"
+            type="name"
+            name="name"
+            onChange={handleChange}
+            value={name}
+            placeholder='John Doe'
+          />
+          <Button disabled={disabled}>ENTER</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+}
---
+++ src/components/LoginForm/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  background-color: white;
+  width: 100%;
+  max-width: 500px;
+  padding: 1.5rem;
+  border-radius: 1rem;
+  margin: 0 auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
