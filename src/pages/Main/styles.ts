214dc61d29c5ebd955d5ef2509d14196c5849ca3
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-04-10 10:20:15 -0300

    Refactor SignUp

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -14,13 +14,3 @@ export const Header = styled.div`
   color: white;
 `;
 
-export const FormContainer = styled.div`
-  padding: 2rem;
-  border: 1px solid #999999;
-  border-radius: 16px;
-  margin: 24px;
-`;
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
